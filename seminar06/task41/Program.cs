92099681a69e8e90fb90eb4a2a559a489c1ff751
MariaUlianova <mari89kurgan@gmail.com>
2022-09-11 14:50:54 +0300

    seminar06 task41

---
+++ seminar06/task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        array[i] = number;
+    }
+    return array;
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Количество чисел в массиве больше нуля: {PositiveNumbers(GetArray(m))}");
