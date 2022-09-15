08f331389bc92487d768e8b0e7513d482519d8ab
MariaUlianova <mari89kurgan@gmail.com>
2022-09-15 16:50:52 +0300

    seminar07 task52

---
+++ seminar07/task52/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+ 
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+ 
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+ 
+    return result;
+}
+ 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+void GetAverage(int[,] array)
+{
+    string result = "";
+ 
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        result += Math.Round(sum / array.GetLength(0), 2) + "\t";
+    }
+    Console.WriteLine(result);
+}
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+ 
+int[,] myArray = GetArray(m, n);
+PrintArray(myArray);
+Console.WriteLine();
+GetAverage(myArray);
